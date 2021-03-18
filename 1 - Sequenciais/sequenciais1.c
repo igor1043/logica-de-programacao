9cbbb239817da675f760a8a2c71cbb4a7a352514
igor vinicius Freitas de Souza <igorviniciusfreitasouza@gmail.com>
2021-03-18 07:38:17 -0300

    Update sequenciais1.c

--- 1 - Sequenciais/sequenciais1.c
+++ 1 - Sequenciais/sequenciais1.c
@@ -1,5 +1,11 @@
 //Faça um programa que mostre a mensagem "Alo mundo" na tela.
 
+/*
+* Author:  Igor Vinicius Freitas de Souza
+* GitHub: https://github.com/igor1043
+* E-mail: igorviniciusfreitasouza@gmail.com
+*/
+
 #include "stdio.h"
 
 int main()
