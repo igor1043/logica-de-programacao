5940778d6e761f5f6fb863d896c8915d20c75952
igor vinicius Freitas de Souza <igorviniciusfreitasouza@gmail.com>
2021-03-19 10:29:53 -0300

    Update sequenciais1.rb

--- 1 - Sequenciais/sequenciais1.rb
+++ 1 - Sequenciais/sequenciais1.rb
@@ -1,6 +1,7 @@
 # Author:  Igor Vinicius Freitas de Souza
 # GitHub: https://github.com/igor1043
 # E-mail: igorviniciusfreitasouza@gmail.com
+
 #Faça um programa que mostre a mensagem "Alo mundo" na tela.
 
 puts "Alo mundo"
