aec0dc3bf11eb494ab41f02956893245bfc30419
igor vinicius Freitas de Souza <igorviniciusfreitasouza@gmail.com>
2021-03-19 10:28:40 -0300

    Update sequenciais1.py

--- 1 - Sequenciais/sequenciais1.py
+++ 1 - Sequenciais/sequenciais1.py
@@ -1,3 +1,6 @@
+# Author:  Igor Vinicius Freitas de Souza
+# GitHub: https://github.com/igor1043
+# E-mail: igorviniciusfreitasouza@gmail.com
 #Faça um programa que mostre a mensagem "Alo mundo" na tela.
 
 print("Alo mundo")
