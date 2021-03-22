05255ee3d08af8c0885d921a470cc441716c7611
igor vinicius Freitas de Souza <igorviniciusfreitasouza@gmail.com>
2021-03-22 07:45:38 -0300

    Update sequenciais10.rb

--- 1 - Sequenciais/sequenciais10.rb
+++ 1 - Sequenciais/sequenciais10.rb
@@ -2,6 +2,7 @@
 # Author:  Igor Vinicius Freitas de Souza
 # GitHub: https://github.com/igor1043
 # E-mail: igorviniciusfreitasouza@gmail.com
+
 puts "Digite uma temperatura, em Celsius: "
 celsius = gets.to_f
 farenheit = 1.80 * celsius + 32
