5e465b537b13decf51cddbe56d2b45f2ff6e8d9a
igor vinicius Freitas de Souza <igorviniciusfreitasouza@gmail.com>
2021-03-23 07:32:59 -0300

    Update sequenciais11.rb

--- 1 - Sequenciais/sequenciais11.rb
+++ 1 - Sequenciais/sequenciais11.rb
@@ -2,6 +2,9 @@
 #  - o produto do dobro do primeiro com metade do segundo
 #  - a soma do triplo do primeiro com o terceiro
 #  - o terceiro elevado ao cubo
+# Author:  Igor Vinicius Freitas de Souza
+# GitHub: https://github.com/igor1043
+# E-mail: igorviniciusfreitasouza@gmail.com
 
 puts "Digite dois numeros inteiros: "
 n1 = gets.to_i
