47cf0acc8c8b6d217eba7a4527c8a3d0da9445f6
igor vinicius Freitas de Souza <igorviniciusfreitasouza@gmail.com>
2021-03-24 07:30:34 -0300

    Update sequenciais12.rb

--- 1 - Sequenciais/sequenciais12.rb
+++ 1 - Sequenciais/sequenciais12.rb
@@ -1,6 +1,8 @@
 #Tendo como dados de entrada a altura de uma pessoa, construa um algoritmo que calcule seu peso ideal, usando a seguinte fórmula:
 #(72.7 * altura) - 58.
-
+# Author:  Igor Vinicius Freitas de Souza
+# GitHub: https://github.com/igor1043
+# E-mail: igorviniciusfreitasouza@gmail.com
 puts "Digite sua altura: "
 altura = gets.to_f
 pesoideal = (72.7 * altura) - 58
