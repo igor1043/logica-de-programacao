c569932a1787bdadbe53be2cbac7497c5f71de00
igor vinicius Freitas de Souza <igorviniciusfreitasouza@gmail.com>
2021-03-25 08:36:51 -0300

    Update sequenciais13.rb

--- 1 - Sequenciais/sequenciais13.rb
+++ 1 - Sequenciais/sequenciais13.rb
@@ -1,5 +1,4 @@
 #Escreva um programa que leia um número real e faça o arredondamento para inteiro.
-
 #Se a parte fracionária for maior do que 0.5 o arredondamento deve ser feito para o próximo inteiro.
 # Author:  Igor Vinicius Freitas de Souza
 # GitHub: https://github.com/igor1043
