22fa4a449eb47def80dab1bca3c0c101ca61f209
igor vinicius Freitas de Souza <igorviniciusfreitasouza@gmail.com>
2021-03-25 08:05:45 -0300

    Update sequenciais13.php

--- 1 - Sequenciais/sequenciais13.php
+++ 1 - Sequenciais/sequenciais13.php
@@ -2,7 +2,11 @@
 
 #Escreva um programa que leia um número real e faça o arredondamento para inteiro.
 #Se a parte fracionária for maior do que 0.5 o arredondamento deve ser feito para o próximo inteiro.
-
+/*
+* Author:  Igor Vinicius Freitas de Souza
+* GitHub: https://github.com/igor1043
+* E-mail: igorviniciusfreitasouza@gmail.com
+*/
 ?>
 
 <!DOCTYPE html>
