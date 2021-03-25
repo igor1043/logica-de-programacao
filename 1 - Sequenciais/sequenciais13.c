53b93fe3b8eb987fb08aa0c36daac9ed8629b176
igor vinicius Freitas de Souza <igorviniciusfreitasouza@gmail.com>
2021-03-25 08:04:40 -0300

    Update sequenciais13.c

--- 1 - Sequenciais/sequenciais13.c
+++ 1 - Sequenciais/sequenciais13.c
@@ -1,6 +1,12 @@
 //Escreva um programa que leia um número real e faça o arredondamento para inteiro.
 //Se a parte fracionária for maior do que 0.5 o arredondamento deve ser feito para o próximo inteiro.
 
+/*
+* Author:  Igor Vinicius Freitas de Souza
+* GitHub: https://github.com/igor1043
+* E-mail: igorviniciusfreitasouza@gmail.com
+*/
+
 #include <stdio.h>
 #include <math.h>
 
