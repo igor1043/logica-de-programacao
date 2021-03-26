dc833dd3f355b7334a883ee83e4f29e73194dcf0
igor vinicius Freitas de Souza <igorviniciusfreitasouza@gmail.com>
2021-03-26 07:51:30 -0300

    Update sequenciais2.php

--- 1 - Sequenciais/sequenciais2.php
+++ 1 - Sequenciais/sequenciais2.php
@@ -1,5 +1,9 @@
 <?php #Faça um programa que peça um número e então mostre a mensagem: "O número informado foi {numero}. ?>
-
+/*
+* Author:  Igor Vinicius Freitas de Souza
+* GitHub: https://github.com/igor1043
+* E-mail: igorviniciusfreitasouza@gmail.com
+*/
 <!DOCTYPE html>
 <html>
   <head>
