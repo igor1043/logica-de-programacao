0249d7d4bec9294969a7c92d768fcdc69b603bd4
igor vinicius Freitas de Souza <igorviniciusfreitasouza@gmail.com>
2021-03-26 07:51:02 -0300

    Update sequenciais2.java

--- 1 - Sequenciais/sequenciais2.java
+++ 1 - Sequenciais/sequenciais2.java
@@ -1,4 +1,9 @@
 //Faça um programa que peça um número e então mostre a mensagem: "O número informado foi {numero}.
+/*
+* Author:  Igor Vinicius Freitas de Souza
+* GitHub: https://github.com/igor1043
+* E-mail: igorviniciusfreitasouza@gmail.com
+*/
 
 import java.util.Scanner;
 
