79d2704115b625698e0cc10745c36ad342f96b63
igor vinicius Freitas de Souza <igorviniciusfreitasouza@gmail.com>
2021-03-26 07:52:37 -0300

    Update sequenciais2.rb

--- 1 - Sequenciais/sequenciais2.rb
+++ 1 - Sequenciais/sequenciais2.rb
@@ -1,5 +1,8 @@
 #Faça um programa que peça um número e então mostre a mensagem: "O número informado foi {numero}.
 
+# Author:  Igor Vinicius Freitas de Souza
+# GitHub: https://github.com/igor1043
+# E-mail: igorviniciusfreitasouza@gmail.com
 puts "Digite um número: "
 numero = gets.to_i
 puts "O número informado foi #{numero}"
