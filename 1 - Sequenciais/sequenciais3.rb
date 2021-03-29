0dcb0adc89c3cc098d2747b91a5ac890352093af
igor vinicius Freitas de Souza <igorviniciusfreitasouza@gmail.com>
2021-03-29 08:48:59 -0300

    Update sequenciais3.rb

--- 1 - Sequenciais/sequenciais3.rb
+++ 1 - Sequenciais/sequenciais3.rb
@@ -1,5 +1,7 @@
 #Faça um programa que peça dois números e imprima a soma.
-
+# Author:  Igor Vinicius Freitas de Souza
+# GitHub: https://github.com/igor1043
+# E-mail: igorviniciusfreitasouza@gmail.com
 puts "Digite dois números: "
 num1 = gets.to_i
 num2 = gets.to_i
