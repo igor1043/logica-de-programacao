186ce7219c887ec7de3a5c7f14a95a652da69d14
igor vinicius Freitas de Souza <igorviniciusfreitasouza@gmail.com>
2021-04-01 08:27:54 -0300

    Update sequenciais6.rb

--- 1 - Sequenciais/sequenciais6.rb
+++ 1 - Sequenciais/sequenciais6.rb
@@ -1,5 +1,7 @@
 #Faça um programa que peça o raio de um círculo, calcule e mostre sua área.
-
+# Author:  Igor Vinicius Freitas de Souza
+# GitHub: https://github.com/igor1043
+# E-mail: igorviniciusfreitasouza@gmail.com
 puts "Digite o raio da circunferência: "
 raio = gets.to_f
 area = (Math::PI * (raio ** 2)).round(2)
