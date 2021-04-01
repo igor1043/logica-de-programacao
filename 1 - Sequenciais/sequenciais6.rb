718737c1f50962c538dfa4ad9ddacee355a15378
igor vinicius Freitas de Souza <igorviniciusfreitasouza@gmail.com>
2021-04-01 08:28:21 -0300

    Update sequenciais6.rb

--- 1 - Sequenciais/sequenciais6.rb
+++ 1 - Sequenciais/sequenciais6.rb
@@ -3,6 +3,7 @@
 # Author:  Igor Vinicius Freitas de Souza
 # GitHub: https://github.com/igor1043
 # E-mail: igorviniciusfreitasouza@gmail.com
+
 puts "Digite o raio da circunferência: "
 raio = gets.to_f
 area = (Math::PI * (raio ** 2)).round(2)
