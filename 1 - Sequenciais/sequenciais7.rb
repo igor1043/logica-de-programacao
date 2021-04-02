6e67dc5d883aa887e05c537ebbba76eabf164559
igor vinicius Freitas de Souza <igorviniciusfreitasouza@gmail.com>
2021-04-02 09:32:44 -0300

    Update sequenciais7.rb

--- 1 - Sequenciais/sequenciais7.rb
+++ 1 - Sequenciais/sequenciais7.rb
@@ -1,5 +1,7 @@
 #Faça um programa que calcule a área de um quadrado, em seguida mostre o dobro desta área para o usuário.
-
+# Author:  Igor Vinicius Freitas de Souza
+# GitHub: https://github.com/igor1043
+# E-mail: igorviniciusfreitasouza@gmail.com
 puts "Digite o tamanho do lado de um quadrado: "
 lado = gets.to_f
 area = lado ** 2
