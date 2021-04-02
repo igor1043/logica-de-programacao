3328c2c5ba6f0e4db05def98cbb467a79b0e44f0
igor vinicius Freitas de Souza <igorviniciusfreitasouza@gmail.com>
2021-04-02 09:30:39 -0300

    Update sequenciais7.java

--- 1 - Sequenciais/sequenciais7.java
+++ 1 - Sequenciais/sequenciais7.java
@@ -1,5 +1,9 @@
 //Faça um programa que calcule a área de um quadrado, em seguida mostre o dobro desta área para o usuário.
-
+/*
+* Author:  Igor Vinicius Freitas de Souza
+* GitHub: https://github.com/igor1043
+* E-mail: igorviniciusfreitasouza@gmail.com
+*/
 import java.util.Scanner;
 
 public class sequenciais7 {
