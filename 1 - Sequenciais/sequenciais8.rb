686c41a8fbe7092cddabe8528c95c68798cbdbc0
igor vinicius Freitas de Souza <igorviniciusfreitasouza@gmail.com>
2021-04-03 09:04:22 -0300

    Update sequenciais8.rb

--- 1 - Sequenciais/sequenciais8.rb
+++ 1 - Sequenciais/sequenciais8.rb
@@ -1,6 +1,8 @@
 #Faça um programa que pergunte quanto você ganha por hora e o número de horas trabalhadas no mês.
 #Calcule e mostre o total do seu salário no referido mês.
-
+# Author:  Igor Vinicius Freitas de Souza
+# GitHub: https://github.com/igor1043
+# E-mail: igorviniciusfreitasouza@gmail.com
 puts "Qual o valor da sua hora de trabalho?"
 valordahora = gets.to_f
 puts "Quantas horas você trabalha por dia?"
