ac9c81e54f4eda5c15efbbdb345cf49ac980dca0
igor vinicius Freitas de Souza <igorviniciusfreitasouza@gmail.com>
2021-04-03 09:02:16 -0300

    Update sequenciais8.java

--- 1 - Sequenciais/sequenciais8.java
+++ 1 - Sequenciais/sequenciais8.java
@@ -1,5 +1,6 @@
 //Faça um programa que pergunte quanto você ganha por hora e o número de horas trabalhadas no mês.
 //Calcule e mostre o total do seu salário no referido mês.
+
 /*
 * Author:  Igor Vinicius Freitas de Souza
 * GitHub: https://github.com/igor1043
