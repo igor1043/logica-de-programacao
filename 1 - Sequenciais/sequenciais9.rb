b3479fbccc9aa4271449a0e950dca8e06c9728e4
igor vinicius Freitas de Souza <igorviniciusfreitasouza@gmail.com>
2021-04-04 08:25:22 -0300

    Update sequenciais9.rb

--- 1 - Sequenciais/sequenciais9.rb
+++ 1 - Sequenciais/sequenciais9.rb
@@ -1,6 +1,8 @@
 #Faça um programa que peça a temperatura em graus Farenheit, transforme e mostre a temperatura em graus Celsius.
 #C = (5 * (F-32) / 9)
-
+# Author:  Igor Vinicius Freitas de Souza
+# GitHub: https://github.com/igor1043
+# E-mail: igorviniciusfreitasouza@gmail.com
 puts "Digite uma temperatura, em Farenheit: "
 farenheit = gets.to_f
 celsius = (5 * (farenheit - 32)) / 9
