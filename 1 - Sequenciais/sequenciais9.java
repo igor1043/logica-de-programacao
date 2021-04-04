14c07fe6147a0c4263ec99b25ae08989332d968b
igor vinicius Freitas de Souza <igorviniciusfreitasouza@gmail.com>
2021-04-04 08:23:55 -0300

    Update sequenciais9.java

--- 1 - Sequenciais/sequenciais9.java
+++ 1 - Sequenciais/sequenciais9.java
@@ -1,5 +1,6 @@
 //Faça um programa que peça a temperatura em graus Farenheit, transforme e mostre a temperatura em graus Celsius.
 //C = (5 * (F-32) / 9)
+
 /*
 * Author:  Igor Vinicius Freitas de Souza
 * GitHub: https://github.com/igor1043
