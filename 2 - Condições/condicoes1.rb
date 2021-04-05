0cc21b076f15103142a2a90709a9a7a234e710f2
igor vinicius Freitas de Souza <igorviniciusfreitasouza@gmail.com>
2021-04-05 07:59:41 -0300

    Update condicoes1.rb

--- 2 - Condições/condicoes1.rb
+++ 2 - Condições/condicoes1.rb
@@ -1,5 +1,7 @@
 #Escreva um programa que leia um número e mostre se ele é igual a 10.
-
+# Author:  Igor Vinicius Freitas de Souza
+# GitHub: https://github.com/igor1043
+# E-mail: igorviniciusfreitasouza@gmail.com
 numero = gets.to_i
 if (numero == 10)
 	puts "O valor é igual a 10."
