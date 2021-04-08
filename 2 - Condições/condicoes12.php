680021a21b25f48c9242ddfa67fee1f1de8e1f7f
igor vinicius Freitas de Souza <igorviniciusfreitasouza@gmail.com>
2021-04-08 07:42:29 -0300

    Update condicoes12.php

--- 2 - Condições/condicoes12.php
+++ 2 - Condições/condicoes12.php
@@ -1,7 +1,9 @@
 <?php
 
 #Brincadeira do ímpar ou par: escreva um programa que leia dois nomes e dois valores inteiros, que correspondem ao que cada um colocou, e informe quem ganhou o "impar ou par".
-
+#Author:  Igor Vinicius Freitas de Souza
+#GitHub: https://github.com/igor1043
+# E-mail: igorviniciusfreitasouza@gmail.com
 ?>
 
 <!DOCTYPE html>
