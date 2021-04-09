b70ad3626d69d3b3dbd624b799e6f708fdab23d9
igor vinicius Freitas de Souza <igorviniciusfreitasouza@gmail.com>
2021-04-09 08:40:59 -0300

    Update condicoes13.php

--- 2 - Condições/condicoes13.php
+++ 2 - Condições/condicoes13.php
@@ -1,7 +1,9 @@
 <?php
 
 #Escreva um programa que leia 4 números e mostre o maior.
-
+#Author:  Igor Vinicius Freitas de Souza
+#GitHub: https://github.com/igor1043
+# E-mail: igorviniciusfreitasouza@gmail.com
 ?>
 
 <!DOCTYPE html>
