bd8fc8d8ce5044fb0575273528ca6236f7339ad1
igor vinicius Freitas de Souza <igorviniciusfreitasouza@gmail.com>
2021-04-12 07:36:14 -0300

    Update condicoes2.rb

--- 2 - Condições/condicoes2.rb
+++ 2 - Condições/condicoes2.rb
@@ -3,6 +3,7 @@
 #Author:  Igor Vinicius Freitas de Souza
 #GitHub: https://github.com/igor1043
 # E-mail: igorviniciusfreitasouza@gmail.com
+
 numero = gets.to_i
 if (numero % 7 == 0)
 	puts "#{numero} é múltiplo de 7."
