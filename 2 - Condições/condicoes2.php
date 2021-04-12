2a966628483aeefe296dcbddc02a6b22e3bf316d
igor vinicius Freitas de Souza <igorviniciusfreitasouza@gmail.com>
2021-04-12 07:35:34 -0300

    Update condicoes2.php

--- 2 - Condições/condicoes2.php
+++ 2 - Condições/condicoes2.php
@@ -1,6 +1,7 @@
 <?php
 
 #Escreva um programa que leia um número e mostre se ele é múltiplo de 7.
+
 #Author:  Igor Vinicius Freitas de Souza
 #GitHub: https://github.com/igor1043
 # E-mail: igorviniciusfreitasouza@gmail.com
