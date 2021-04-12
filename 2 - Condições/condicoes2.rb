0815f84306e9ad80a85f1532ee4eb5abc0c32984
igor vinicius Freitas de Souza <igorviniciusfreitasouza@gmail.com>
2021-04-12 07:35:55 -0300

    Update condicoes2.rb

--- 2 - Condições/condicoes2.rb
+++ 2 - Condições/condicoes2.rb
@@ -1,5 +1,7 @@
 #Escreva um programa que leia um número e mostre se ele é múltiplo de 7.
-
+#Author:  Igor Vinicius Freitas de Souza
+#GitHub: https://github.com/igor1043
+# E-mail: igorviniciusfreitasouza@gmail.com
 numero = gets.to_i
 if (numero % 7 == 0)
 	puts "#{numero} é múltiplo de 7."
