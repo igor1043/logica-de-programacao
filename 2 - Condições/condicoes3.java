5a3124bbc3f381b40198e98623dcf402d293e51a
igor vinicius Freitas de Souza <igorviniciusfreitasouza@gmail.com>
2021-04-13 07:37:52 -0300

    Update condicoes3.java

--- 2 - Condições/condicoes3.java
+++ 2 - Condições/condicoes3.java
@@ -1,5 +1,9 @@
 //Escreva um programa que leia um número e mostre se ele é positivo.
-
+/*
+* Author:  Igor Vinicius Freitas de Souza
+* GitHub: https://github.com/igor1043
+* E-mail: igorviniciusfreitasouza@gmail.com
+*/
 import java.util.Scanner;
 
 public class condicoes3 {
