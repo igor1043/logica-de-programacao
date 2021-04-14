b4e39bc3d91e7cb95d7e1707943ec79b532fe077
igor vinicius Freitas de Souza <igorviniciusfreitasouza@gmail.com>
2021-04-14 07:24:26 -0300

    Update condicoes4.rb

--- 2 - Condições/condicoes4.rb
+++ 2 - Condições/condicoes4.rb
@@ -1,5 +1,7 @@
 #Escreva um programa que leia dois números e informe se eles são iguais.
-
+# Author:  Igor Vinicius Freitas de Souza
+# GitHub: https://github.com/igor1043
+# E-mail: igorviniciusfreitasouza@gmail.com
 numero1 = gets.to_i
 numero2 = gets.to_i
 if (numero1 == numero2)
