2f3a5685e0c8491890f8d6117738b42064a75ca7
igor vinicius Freitas de Souza <igorviniciusfreitasouza@gmail.com>
2021-04-16 07:10:20 -0300

    Update condicoes5.c

--- 2 - Condições/condicoes5.c
+++ 2 - Condições/condicoes5.c
@@ -1,4 +1,9 @@
 //Escreva um programa que leia dois números e mostre o maior.
+/*
+* Author:  Igor Vinicius Freitas de Souza
+* GitHub: https://github.com/igor1043
+* E-mail: igorviniciusfreitasouza@gmail.com
+*/
 
 #include <stdio.h>
 
