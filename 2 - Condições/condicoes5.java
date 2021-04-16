ab408a331784c5bf1b5f03317f807f83a67f4bf3
igor vinicius Freitas de Souza <igorviniciusfreitasouza@gmail.com>
2021-04-16 07:10:50 -0300

    Update condicoes5.java

--- 2 - Condições/condicoes5.java
+++ 2 - Condições/condicoes5.java
@@ -1,5 +1,9 @@
 //Escreva um programa que leia dois números e mostre o maior.
-
+/*
+* Author:  Igor Vinicius Freitas de Souza
+* GitHub: https://github.com/igor1043
+* E-mail: igorviniciusfreitasouza@gmail.com
+*/
 import java.util.Scanner;
 
 public class condicoes5 {
