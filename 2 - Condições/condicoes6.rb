0b4c627ac31cf391c9630bc8051fe4215ef5bcd2
igor vinicius Freitas de Souza <igorviniciusfreitasouza@gmail.com>
2021-04-20 07:43:27 -0300

    Update condicoes6.rb

--- 2 - Condições/condicoes6.rb
+++ 2 - Condições/condicoes6.rb
@@ -1,5 +1,7 @@
 #Escreva um programa que leia dois nomes e mostre o que contém maior quantidade de caracteres.
-
+# Author:  Igor Vinicius Freitas de Souza
+# GitHub: https://github.com/igor1043
+# E-mail: igorviniciusfreitasouza@gmail.com
 nome1 = gets.chomp
 nome2 = gets.chomp
 if (nome1.length >= nome2.length)
