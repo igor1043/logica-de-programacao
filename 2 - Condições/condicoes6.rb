7d3b36b183d5079742165dd5bde26b09799046c8
igor vinicius Freitas de Souza <igorviniciusfreitasouza@gmail.com>
2021-04-20 07:43:41 -0300

    Update condicoes6.rb

--- 2 - Condições/condicoes6.rb
+++ 2 - Condições/condicoes6.rb
@@ -3,6 +3,7 @@
 # Author:  Igor Vinicius Freitas de Souza
 # GitHub: https://github.com/igor1043
 # E-mail: igorviniciusfreitasouza@gmail.com
+
 nome1 = gets.chomp
 nome2 = gets.chomp
 if (nome1.length >= nome2.length)
