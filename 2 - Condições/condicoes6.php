1b0d02d448dc8493508282e1a7699e7e2b2b5915
igor vinicius Freitas de Souza <igorviniciusfreitasouza@gmail.com>
2021-04-20 07:43:02 -0300

    Update condicoes6.php

--- 2 - Condições/condicoes6.php
+++ 2 - Condições/condicoes6.php
@@ -1,7 +1,9 @@
 <?php
 
 #Escreva um programa que leia dois nomes e mostre o que contém maior quantidade de caracteres.
-
+# Author:  Igor Vinicius Freitas de Souza
+# GitHub: https://github.com/igor1043
+# E-mail: igorviniciusfreitasouza@gmail.com
 ?>
 
 <!DOCTYPE html>
