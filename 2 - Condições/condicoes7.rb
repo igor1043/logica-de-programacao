b5bd45682b4dea387fdd58f73bb25368099bdc48
igor vinicius Freitas de Souza <igorviniciusfreitasouza@gmail.com>
2021-04-21 07:59:07 -0300

    Update condicoes7.rb

--- 2 - Condições/condicoes7.rb
+++ 2 - Condições/condicoes7.rb
@@ -1,5 +1,7 @@
 #Escreva um programa que leia um número e mostre se ele é positivo.
-
+# Author:  Igor Vinicius Freitas de Souza
+# GitHub: https://github.com/igor1043
+# E-mail: igorviniciusfreitasouza@gmail.com
 numero = gets.to_i
 if (numero >= 0)
 	puts "O numero #{numero} é positivo."
