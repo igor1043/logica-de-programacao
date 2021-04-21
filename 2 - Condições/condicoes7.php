47e29568d571386ca0db9399b3eb49ac5f44d465
igor vinicius Freitas de Souza <igorviniciusfreitasouza@gmail.com>
2021-04-21 07:58:44 -0300

    Update condicoes7.php

--- 2 - Condições/condicoes7.php
+++ 2 - Condições/condicoes7.php
@@ -1,7 +1,9 @@
 <?php
 
 #Escreva um programa que leia um número e mostre se ele é positivo.
-
+# Author:  Igor Vinicius Freitas de Souza
+# GitHub: https://github.com/igor1043
+# E-mail: igorviniciusfreitasouza@gmail.com
 ?>
 
 <!DOCTYPE html>
