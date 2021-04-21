03d1b0e17a2e9da1ffcff66d847bb855e102c96a
igor vinicius Freitas de Souza <igorviniciusfreitasouza@gmail.com>
2021-04-21 07:59:16 -0300

    Update condicoes7.rb

--- 2 - Condições/condicoes7.rb
+++ 2 - Condições/condicoes7.rb
@@ -1,4 +1,5 @@
 #Escreva um programa que leia um número e mostre se ele é positivo.
+
 # Author:  Igor Vinicius Freitas de Souza
 # GitHub: https://github.com/igor1043
 # E-mail: igorviniciusfreitasouza@gmail.com
