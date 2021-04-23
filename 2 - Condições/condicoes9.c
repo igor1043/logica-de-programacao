9fa5bf4b3cf2f403b3b4f908adb68feccc6119ab
igor vinicius Freitas de Souza <igorviniciusfreitasouza@gmail.com>
2021-04-23 07:42:41 -0300

    Update condicoes9.c

--- 2 - Condições/condicoes9.c
+++ 2 - Condições/condicoes9.c
@@ -1,5 +1,9 @@
 //Escreva um programa que leia um número inteiro que corresponde a um ângulo e informe em qual quadrante este ângulo se encontra e quantas voltas ele dá. (360º = uma volta)
-
+/*
+* Author:  Igor Vinicius Freitas de Souza
+* GitHub: https://github.com/igor1043
+* E-mail: igorviniciusfreitasouza@gmail.com
+*/
 #include <stdio.h>
 
 int main() {
