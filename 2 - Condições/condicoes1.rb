54245b576c9c6652cad17171fc9edc9e8c78dbc6
igor vinicius Freitas de Souza <igorviniciusfreitasouza@gmail.com>
2021-04-25 14:46:08 -0300

    Update condicoes1.rb

--- 2 - Condições/condicoes1.rb
+++ 2 - Condições/condicoes1.rb
@@ -1,5 +1,4 @@
 #Escreva um programa que leia um número e mostre se ele é igual a 10.
-
 # Author:  Igor Vinicius Freitas de Souza
 # GitHub: https://github.com/igor1043
 # E-mail: igorviniciusfreitasouza@gmail.com
