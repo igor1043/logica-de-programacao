f9bb00588609b6d299b247df65f95020a16c145d
igor vinicius Freitas de Souza <igorviniciusfreitasouza@gmail.com>
2021-04-28 07:11:55 -0300

    Update lacos1.rb

--- 3 - Laços/lacos1.rb
+++ 3 - Laços/lacos1.rb
@@ -1,6 +1,8 @@
 #Faça um programa que peça uma nota, entre zero e dez.
 #Mostre uma mensagem caso o valor seja inválido e continue pedindo até que o usuário informe um valor válido
-
+# Author:  Igor Vinicius Freitas de Souza
+# GitHub: https://github.com/igor1043
+# E-mail: igorviniciusfreitasouza@gmail.com
 numero = -1
 
 while (numero < 0 || numero > 10) do
