505c92fd57eebaa9f87b472a87e027510dafa11b
igor vinicius Freitas de Souza <igorviniciusfreitasouza@gmail.com>
2021-04-29 07:16:29 -0300

    Update lacos1.php

--- 3 - Laços/lacos1.php
+++ 3 - Laços/lacos1.php
@@ -2,11 +2,9 @@
 
 #Faça um programa que peça uma nota, entre zero e dez.
 #Mostre uma mensagem caso o valor seja inválido e continue pedindo até que o usuário informe um valor válido
-
 # Author:  Igor Vinicius Freitas de Souza
 # GitHub: https://github.com/igor1043
 # E-mail: igorviniciusfreitasouza@gmail.com
-
 ?>
 
 <!DOCTYPE html>
