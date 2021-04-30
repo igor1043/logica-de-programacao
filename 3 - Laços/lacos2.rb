e9f20b973761f8c9e5a455c8dbcb3188089d8440
igor vinicius Freitas de Souza <igorviniciusfreitasouza@gmail.com>
2021-04-30 07:41:54 -0300

    Update lacos2.rb

--- 3 - Laços/lacos2.rb
+++ 3 - Laços/lacos2.rb
@@ -1,6 +1,8 @@
 #Faça um programa que leia um nome de usuário e a sua senha e não aceite a senha igual ao nome do usuário,
 #mostrando uma mensagem de erro e voltando a pedir as informações.
-
+# Author:  Igor Vinicius Freitas de Souza
+# GitHub: https://github.com/igor1043
+# E-mail: igorviniciusfreitasouza@gmail.com
 loop do
   puts "Usuário:"
   user = gets.to_s
