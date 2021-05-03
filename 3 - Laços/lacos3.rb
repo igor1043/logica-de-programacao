92a7182d440c4158c2f712af757c37e63c6c0356
igor vinicius Freitas de Souza <igorviniciusfreitasouza@gmail.com>
2021-05-03 07:25:57 -0300

    Update lacos3.rb

--- 3 - Laços/lacos3.rb
+++ 3 - Laços/lacos3.rb
@@ -1,5 +1,7 @@
 #Faça um programa que leia 5 números e informe o maior número.
-
+# Author:  Igor Vinicius Freitas de Souza
+# GitHub: https://github.com/igor1043
+# E-mail: igorviniciusfreitasouza@gmail.com
 maior = 0
 for i in 1..5 do
   num = gets.to_i
