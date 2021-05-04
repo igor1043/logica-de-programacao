be4abf178ca44e9d76622886a488411ef191d40f
igor vinicius Freitas de Souza <igorviniciusfreitasouza@gmail.com>
2021-05-04 07:04:40 -0300

    Update lacos4.rb

--- 3 - Laços/lacos4.rb
+++ 3 - Laços/lacos4.rb
@@ -1,5 +1,7 @@
 #Faça um programa que leia 5 números e informe a soma e a média dos números.
-
+# Author:  Igor Vinicius Freitas de Souza
+# GitHub: https://github.com/igor1043
+# E-mail: igorviniciusfreitasouza@gmail.com
 soma = 0
 for i in 1..5 do
   num = gets.to_i
