582bee3272ab539ff748942c38c782bcee8b99d0
igor vinicius Freitas de Souza <igorviniciusfreitasouza@gmail.com>
2021-05-06 07:59:01 -0300

    Update sequenciais1.cs

--- 1 - Sequenciais/sequenciais1.cs
+++ 1 - Sequenciais/sequenciais1.cs
@@ -1,6 +1,5 @@
 //Faça um programa que mostre a mensagem "Alo mundo" na tela.
 /*
-
 * Author:  Igor Vinicius Freitas de Souza
 * GitHub: https://github.com/igor1043
 * E-mail: igorviniciusfreitasouza@gmail.com
